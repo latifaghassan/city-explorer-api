8033daeb39fbd85a7cd4d6358cc729872efb895b
latifaghassan <lalooshamayra@hotmail.com>
2021-06-19 15:34:02 +0300

    finished lab10

--- controller/movies.controller.js
+++ controller/movies.controller.js
@@ -2,22 +2,36 @@ const axios = require("axios");
 const Movies = require("../models/movies.model");
 require("dotenv").config();
 
+const Cache = require("../helper/Cache");
+const cacheObj = new Cache();
+
 const movieController = (req, res) => {
   const key = process.env.MOVIE_API_KEY;
   let cityName = req.query.searchQuery;
+  const requestKey = `movie-${cityName}`;
 
-  let url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&region=${cityName}`;
-  axios
-    .get(url)
-    .then((response) => {
-      let result = response.data.results.map((movieObj) => {
-        return new Movies(movieObj);
+  if (
+    cacheObj[requestKey] &&
+    Date.now() - cacheObj[requestKey].timestamp < 86400000
+  ) {
+    res.json(cacheObj[requestKey]);
+  } else {
+    let movieUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&region=${cityName}`;
+    axios
+      .get(movieUrl)
+      .then((response) => {
+        let result = response.data.results.map((movieObj) => {
+          return new Movies(movieObj);
+        });
+        cacheObj[requestKey] = result;
+        cacheObj[requestKey].timestamp = Date.now();
+        res.send(result);
+      })
+      .catch((error) => {
+        res.send(error.message);
       });
-      res.send(result);
-    })
-    .catch((error) => {
-      res.send(error.message);
-    });
+  }
 };
-
 module.exports = movieController;
+
+//http://localhost:3030/movies?&1a9f288fa92954f9268b43212b758b34&searchQuery=us
--- controller/weather.controller.js
+++ controller/weather.controller.js
@@ -2,23 +2,40 @@ const axios = require("axios");
 const Weather = require("../models/weather.model");
 require("dotenv").config();
 
+const Cache = require("../helper/Cache");
+
+const cacheObj = new Cache();
+
 const weatherController = (req, res) => {
   const key = process.env.WEATHER_BIT_KEY;
   const lat = req.query.lat;
   const lon = req.query.lon;
+  const requestKey = `weather-${lat}-${lon}`;
 
-  // what we are doing here is that we stopped reading data from weather.json file and we start to read real information from weather API
-  let weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}`;
-  axios
-    .get(weatherBitUrl)
-    .then((response) => {
-      let result = response.data.data.map((weatherObj) => {
-        return new Weather(weatherObj);
+  if (
+    cacheObj[requestKey] &&
+    Date.now() - cacheObj[requestKey].timestamp < 86400000
+  ) {
+    console.log("btata");
+    res.send(cacheObj[requestKey]);
+  } else {
+    // what we are doing here is that we stopped reading data from weather.json file and we start to read real information from weather API
+    let weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}`;
+    axios
+      .get(weatherBitUrl)
+      .then((response) => {
+        let result = response.data.data.map((weatherObj) => {
+          return new Weather(weatherObj);
+        });
+        cacheObj[requestKey] = result;
+        cacheObj[requestKey].timestamp = Date.now();
+        res.send(result);
+      })
+      .catch((error) => {
+        res.send(error.message);
       });
-      res.send(result);
-    })
-    .catch((error) => {
-      res.send(error.message);
-    });
+  }
 };
 module.exports = weatherController;
+
+//http://localhost:3030/weather?lon=151&lat=-33.87
